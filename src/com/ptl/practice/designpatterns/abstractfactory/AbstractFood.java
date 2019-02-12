443c0fb920693c00d189d395b11c8ff6ed27e934
panta <pantaoling@mytijian.com>
2019-02-12 12:05:57 +0800

    minor

--- src/com/ptl/practice/designpatterns/abstractfactory/AbstractFood.java
+++ src/com/ptl/practice/designpatterns/abstractfactory/AbstractFood.java
@@ -2,7 +2,7 @@ package com.ptl.practice.designpatterns.abstractfactory;
 
 /**
  * created by panta on 2019/2/12.
- *
+ * abstract product
  * @author panta
  */
 public abstract class AbstractFood {
--- src/com/ptl/practice/designpatterns/abstractfactory/AbstractFoodFactory.java
+++ src/com/ptl/practice/designpatterns/abstractfactory/AbstractFoodFactory.java
@@ -2,7 +2,7 @@ package com.ptl.practice.designpatterns.abstractfactory;
 
 /**
  * created by panta on 2019/2/12.
- *
+ *  abstract factory
  * @author panta
  */
 public abstract class AbstractFoodFactory {
